cfeb1f2f68f0c57280e9aaf067a559f0a0f54b35
Naz Naddaf <naz.naddaf@gmail.com>
2018-04-30 18:36:37 +0200

    Initial commit

---
+++ sketch1.js
@@ -0,0 +1,50 @@
+let posX;
+let posY;
+let positionsX = [];
+let positionsY = [];
+let radius = 5;
+
+
+function setup (){
+	createCanvas(innerWidth, innerHeight);
+	posX = width/2;
+	posY = height/2;
+	background(255);
+}
+
+function draw() {
+	//background(255);
+	strokeWeight(radius);
+	for (let i = 0; i < positionsX.length; i++){
+	point(positionsX[i], positionsY[i]);
+}
+
+move();
+let arrayLimit = 300;
+if(positionsX.length > arrayLimit){
+	positionsX.splice (0,1);
+	positionsY.splice (0,1);
+	}
+}
+
+function move() {
+	point(posX, posY);
+	let probability = floor(random(4)); //0-3
+	if (probability ==0){
+		posY -=radius;
+		positionsX.push(posX);
+		positionsX.push(posY);
+	}else if (probability ==1){
+		posX +=radius;
+		positionsX.push(posX);
+		positionsX.push(posY);
+	}else if (probability ==2){
+		posY +=radius;
+		positionsX.push(posX);
+		positionsX.push(posY);
+	}else if (probability ==3){
+		posX -=radius;
+		positionsX.push(posX);
+		positionsX.push(posY);
+	}
+}
